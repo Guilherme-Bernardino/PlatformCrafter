00f32a13c69d9937655b7399326ccbc943ee4825
Guilherme-Bernardino <92990427+Guilherme-Bernardino@users.noreply.github.com>
2024-08-06 13:41:08 +0100

    #12 - Inventory Module v1.0
    -Added an inventory to the system, with Inventory slots, a method to add and remove items.
    - Added InventoryItem scriptable object.
    - Added specific getters using name of the module for Modular Brain.

--- Assets/Script/Modular System/Abstracts/Receptor.cs
+++ Assets/Script/Modular System/Abstracts/Receptor.cs
@@ -7,12 +7,21 @@ using UnityEngine.Events;
 
 public class Receptor : InteractionReceptor
 {
+    public string inventoryModuleName;
+    public int amount;
+    [SerializeField] private ModularBrain mb;
+
+
     protected override void DoInteraction(GameObject obj)
     {
         if (obj == this.gameObject)
         {
-            ////Something
             Debug.Log($"{gameObject.name} interacted with!");
         }
+    }
+
+    public void AddAppleToInventory(InventoryItem item)
+    {
+        item.AddToInventory(mb.GetInventoryTypeModuleByName(inventoryModuleName), amount);
     }
 }
--- Assets/Script/Modular System/InventoryTypeModule.cs
+++ Assets/Script/Modular System/InventoryTypeModule.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 namespace PlatformCrafterModularSystem
 {
@@ -8,14 +10,170 @@ namespace PlatformCrafterModularSystem
     [CreateAssetMenu(fileName = "InventoryModule", menuName = "Platform Crafter's Modular System/Type - Inventory")]
     public class InventoryTypeModule : Module
     {
+        [SerializeField] private int gridWidth = 5;
+        [SerializeField] private int gridHeight = 5;
+        [SerializeField] private List<InventorySlot> inventorySlots = new List<InventorySlot>();
+
         protected override void InitializeModule()
         {
-
+            
         }
 
         public override void UpdateModule()
         {
 
         }
+
+        public void InitializeInventory()
+        {
+            inventorySlots.Clear();
+            for (int i = 0; i < gridWidth * gridHeight; i++)
+            {
+                inventorySlots.Add(new InventorySlot());
+            }
+        }
+
+        public bool AddItem(InventoryItem item, int quantity = 1)
+        {
+            if (item.isStackable)
+            {
+                foreach (var slot in inventorySlots)
+                {
+                    if (slot.Item == item && slot.Quantity < item.maxStackSize)
+                    {
+                        int availableSpace = item.maxStackSize - slot.Quantity;
+                        int amountToAdd = Mathf.Min(quantity, availableSpace);
+                        slot.Quantity += amountToAdd;
+                        quantity -= amountToAdd;
+
+                        if (quantity <= 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            foreach (var slot in inventorySlots)
+            {
+                if (slot.Item == null)
+                {
+                    slot.Item = item;
+                    slot.Quantity = quantity;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool RemoveItem(InventoryItem item, int quantity = 1)
+        {
+            for (int i = 0; i < inventorySlots.Count; i++)
+            {
+                if (inventorySlots[i].Item == item)
+                {
+                    if (inventorySlots[i].Quantity >= quantity)
+                    {
+                        inventorySlots[i].Quantity -= quantity;
+                        if (inventorySlots[i].Quantity == 0)
+                        {
+                            inventorySlots[i].Item = null;
+                        }
+                        return true;
+                    }
+                    else
+                    {
+                        quantity -= inventorySlots[i].Quantity;
+                        inventorySlots[i].Item = null;
+                        inventorySlots[i].Quantity = 0;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public List<InventorySlot> GetInventorySlots()
+        {
+            return inventorySlots;
+        }
+
+    }
+
+    [CustomEditor(typeof(InventoryTypeModule))]
+    public class InventoryTypeModuleEditor : Editor
+    {
+        private InventoryTypeModule inventoryModule;
+        private SerializedProperty gridWidth;
+        private SerializedProperty gridHeight;
+
+        private void OnEnable()
+        {
+            inventoryModule = (InventoryTypeModule)target;
+            gridWidth = serializedObject.FindProperty("gridWidth");
+            gridHeight = serializedObject.FindProperty("gridHeight");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(gridWidth);
+            EditorGUILayout.PropertyField(gridHeight);
+
+            if (GUILayout.Button("Initialize Inventory"))
+            {
+                inventoryModule.InitializeInventory();
+            }
+
+            DrawInventoryGrid();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawInventoryGrid()
+        {
+            int width = gridWidth.intValue;
+            int height = gridHeight.intValue;
+            List<InventorySlot> slots = inventoryModule.GetInventorySlots();
+
+            if (slots == null || slots.Count != width * height)
+            {
+                return;
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                for (int x = 0; x < width; x++)
+                {
+                    int index = y * width + x;
+                    InventorySlot slot = slots[index];
+
+                    GUILayout.BeginVertical("box", GUILayout.Width(30), GUILayout.Height(50));
+                    if (slot.Item != null)
+                    {
+                        if (slot.Item.icon != null)
+                        {
+                            GUILayout.Label(slot.Item.icon.texture, GUILayout.Width(20), GUILayout.Height(20));
+                        }
+                        GUILayout.Label($"{slot.Item.itemName} ({slot.Quantity})");
+                    }
+                    else
+                    {
+                        GUILayout.Label("Empty");
+                    }
+                    GUILayout.EndVertical();
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class InventorySlot
+    {
+        public InventoryItem Item;
+        public int Quantity;
     }
 }
